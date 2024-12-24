dc45d3720f1d041d52edeb0b9bd3c65d7494920e
Carlos Aquije Melgar <enrique.a.melgar@gmail.com>
2024-12-23 21:38:41 -0500

    Initial Commit

---
+++ journal/journalApp.swift
@@ -0,0 +1,17 @@
+//
+//  journalApp.swift
+//  journal
+//
+//  Created by Carlos Aquije on 23/12/24.
+//
+
+import SwiftUI
+
+@main
+struct journalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ journalTests/journalTests.swift
@@ -0,0 +1,17 @@
+//
+//  journalTests.swift
+//  journalTests
+//
+//  Created by Carlos Aquije on 23/12/24.
+//
+
+import Testing
+@testable import journal
+
+struct journalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
