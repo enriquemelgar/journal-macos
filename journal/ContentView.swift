8c242bdb8c50dd79c5fa61884f26c827b9bfaa3b
Carlos Aquije Melgar <enrique.a.melgar@gmail.com>
2024-12-24 15:00:51 -0500

    Initial commit for a macOS app

--- journal/ContentView.swift
+++ journal/ContentView.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var contentEditor = "This is a test"
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("This is going to be a journal app")
+                
+            TextEditor(text: $contentEditor)
         }
         .padding()
     }
